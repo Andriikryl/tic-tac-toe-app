06b5038670dda7f7f82042f8c0e7dc6201235130
Andriikryl <andreyk557@gmail.com>
2023-05-18 19:58:54 +0300

    feat[add more users]

--- components/game/game-info.jsx
+++ components/game/game-info.jsx
@@ -1,35 +1,62 @@
 import clsx from "clsx";
 import { Profile } from "../profile";
-import { CrossIcon } from "./icons/cross-icon";
+import { GameSymbol } from "./game-symbol";
 import { GAME_SYMBOLS } from "./constants";
-import {GameSymbol} from "./constants"
+
+import avatarSrc1 from "./images/avatar-1.png";
+import avatarSrc2 from "./images/avatar-2.png";
+import avatarSrc3 from "./images/avatar-3.png";
+import avatarSrc4 from "./images/avatar-4.png";
 
 const players = [
-  {name: "John", rating: 666, avatar: '', symbol: GAME_SYMBOLS.CROSS}
+  {id:1, name: "John", rating: 666, avatar: avatarSrc1, symbol: GAME_SYMBOLS.CROSS},
+  {id:1, name: "John", rating: 777, avatar: avatarSrc2, symbol: GAME_SYMBOLS.CROSS},
+  {id:1, name: "John", rating: 888, avatar: avatarSrc3, symbol: GAME_SYMBOLS.CROSS},
+  {id:1, name: "John", rating: 999, avatar: avatarSrc4, symbol: GAME_SYMBOLS.CROSS},
 ]
 
-export function GameInfo({className}){
+export function GameInfo({ className, playersCount }) {
   return (
-    <div className={clsx(className, "rounded-lg bg-opacity-60 backdrop-filter backdrop-blur-lg bg-white shadow px-8 py-4 flex justify-between")}>
-
-      <PlayerInfo/>
+    <div
+      className={clsx(
+        className,
+        "bg-white rounded-2xl shadow-md px-8 py-4 justify-between grid grid-cols-2 gap-3"
+      )}
+    >
+      {players.slice(0, playersCount).map((player, index) => (
+        <PlayerInfo
+          key={player.id}
+          playerInfo={player}
+          isRight={index % 2 === 1}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-function PlayerInfo({playerInfo}) {
+function PlayerInfo({ playerInfo, isRight }) {
   return (
-    <div className="flex gap-4 items-center">
-    <div className="relative">
-      <Profile className="w-[180px]" name={playerInfo.name} rating={playerInfo.rating} avatar={playerInfo.avatar}/>
-      <div className="w-7 h-7 rounded-full bg-white shadow absolute -left-1 -top-1 items-center justify-center flex">
-        <GameSymbol symbol={playerInfo.symbol} />
+    <div className="flex gap-3 items-center">
+      <div className={clsx("relative", isRight && "order-3")}>
+        <Profile
+          className="w-44"
+          name={playerInfo.name}
+          rating={playerInfo.rating}
+          avatar={playerInfo.avatar}
+        />
+        <div className="w-5 h-5 rounded-full bg-white shadow absolute -left-1 -top-1 flex items-center justify-center">
+          <GameSymbol symbol={playerInfo.symbol} />
+        </div>
+      </div>
+      <div className={clsx("h-6 w-px bg-slate-200", isRight && "order-2")} />
+      <div
+        className={clsx(
+          "text-slate-900 text-lg font-semibold",
+          isRight && "order-1"
+        )}
+      >
+        01:08
       </div>
     </div>
-    <div className="h-6 w-px bg-black"></div>
-    <div>
-      <p className="text-[30px]">01:08</p>
-    </div>
-  </div>
-  )
+  );
 }
--- components/game/game-symbol.jsx
+++ components/game/game-symbol.jsx
@@ -1,12 +1,8 @@
-
 import { GAME_SYMBOLS } from "./constants";
 import { CrossIcon } from "./icons/cross-icon";
 import { SquareIcon } from "./icons/square-icon";
 import { TringleIcon } from "./icons/tringle-icon";
 import { ZeroIcon } from "./icons/zero-icon";
-
-
-
 
 export function GameSymbol({ symbol, className }) {
   const Icon =
